65e12ff509c1808457b575540778bb4657700c43
Michael Schumacher <schumacher.m@me.com>
2018-01-28 14:12:17 +0100

    Add icon + color.

--- scripts/rpomo.sh
+++ scripts/rpomo.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-echo $(rpomo --status)
+
+WORKING_ICON="💻"
+BREAK_ICON="💤"
+RPOMO=$(rpomo --status)
+COLOR=""
+
+if  [[ $RPOMO == Working* ]] ;
+then
+  ICON=$WORKING_ICON
+  COLOR="#[fg=white]"
+else
+  ICON=$BREAK_ICON
+  COLOR="#[fg=green]"
+fi
+
+echo "$ICON $COLOR $RPOMO"
